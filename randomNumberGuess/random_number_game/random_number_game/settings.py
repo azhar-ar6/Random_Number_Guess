8147293f84e7d2626335cca7ee4667f76ccaec7a
Mohammad Abdul Azhar <azharmohammad6666@gmail.com>
2024-08-23 22:59:53 +0530

    Update settings.py

--- randomNumberGuess/random_number_game/random_number_game/settings.py
+++ randomNumberGuess/random_number_game/random_number_game/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-305s$$ou7axko^*_j*-mjcia*mm1sj@&$)$16$tsr+*2gbn^d=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
